9c6b350f852adfd134a2e2127d979ca73b65ece9
Alberto <dejahvuuu@gmail.com>
2022-01-14 14:58:48 -0500

    Refactoring to redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
-import HelloWorld from "./HelloWorld";
+//import React, { useState } from "react";
+import React from 'react';
+import HelloWorld from './HelloWorld';
+import reducers from './reducers';
+import { createStore } from 'redux';
+
+const initialState = { tech: 'React' };
+const store = createStore(reducers, initialState);
 
 const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
+  //const [tech] = useState('React');
+  return <HelloWorld tech={store.getState().tech} />;
 };
 
 export default App;
